e2a7e23c48a03673d395c301dbac9d6bef50c4b9
= <andersclement96@gmail.com>
2021-04-19 16:06:39 +0200

    added pico recording/visualization

---
+++ pico_visualizer_pcl/build/CMakeFiles/pico_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pico_visualizer.dir/pico_visualizer.cpp.o"
+  "pico_visualizer"
+  "pico_visualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pico_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
